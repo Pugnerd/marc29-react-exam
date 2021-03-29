ecca3f0cd6afb97ed7545ead320465cd5eb71609
PugnerD <pugnerdani@gmail.com>
2021-03-29 11:05:56 +0200

    project backend done

--- backend/backend.js
+++ backend/backend.js
@@ -4,21 +4,15 @@ let fs = require("fs");
 fs.readFile('./movies.json', function(error, data) {
   movieData = JSON.parse(data.toString());
 });
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded(
+{extended: true}))
+const port = 2900
 
-const port = 2700
 
-app.use(function(req, res, next) {
-  console.log('request:', req.url,req.body, req.method);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept, x-token");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-  if(req.method === 'OPTIONS') {
-      res.end();
-  }
-  else {
-      next();
-  }
-});
+
 
 app.get("/movies", (req, res) => {
   res.set("Content-Type", "application/json")
